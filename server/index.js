540241fe4cb3aaa96585c61d84e48e69b15e6baf
davidBilson <davidbilson99@gmail.com>
2024-02-23 11:11:45 +0100

    reconfigured mongodb connnection

--- server/index.js
+++ server/index.js
@@ -1,25 +1,25 @@
-import express from "express"; // Importing Express framework for handling HTTP requests and responses.
-import bodyParser from "body-parser"; // Importing body-parser middleware to parse incoming request bodies.
-import mongoose from "mongoose"; // Importing Mongoose ORM for MongoDB to interact with the database.
-import cors from "cors"; // Importing CORS middleware to handle Cross-Origin Resource Sharing.
-import helmet from "helmet"; // Importing Helmet middleware for securing HTTP headers.
-import multer from "multer"; // Importing Multer middleware for handling multipart/form-data.
-import dotenv from "dotenv"; // Importing dotenv to load environment variables from a .env file.
-import morgan from "morgan"; // Importing Morgan middleware for logging HTTP requests.
-import path from "path"; // Importing path module to manipulate file paths.
-import { fileURLToPath } from "url"; // Importing fileURLToPath function to convert file URLs to file paths.
+// MODULE IMPORT
+import express from "express"; // Handle HTTP requests and responses.
+import bodyParser from "body-parser"; // Parse incoming request bodies.
+import mongoose from "mongoose"; // Allows mongoDB to interact with the database.
+import cors from "cors"; // Handles Cross-Origin Resource Sharing.
+import helmet from "helmet"; // Secure HTTP headers.
+import multer from "multer"; // Handles multipart/form-data.
+import dotenv from "dotenv"; // Loads environment variables from a .env file.
+import morgan from "morgan"; // Logs HTTP requests.
+import path from "path"; // Manipulate file paths.
+import { fileURLToPath } from "url"; // Convert file URLs to file paths.
+// COMPONENT / FILE IMPORT
 import { register } from "./controllers/auth.js"
 
-
-// CONFIGURATIONS
-
+// CONFIGURATIONS //
 // Setting up file and directory paths
 const __filename = fileURLToPath(import.meta.url); // Convert file URL of the current module to a file path.
 const __dirname = path.dirname(__filename); // Get the directory name of the current module's file.
 dotenv.config(); // Load environment variables from .env file into process.env.
 const app = express(); // Create an Express application.
 
-// Middleware setup
+// MIDDLEWARE SETUP //
 app.use(express.json()) // Parsing incoming JSON requests.
 app.use(helmet()); // Setting various HTTP headers for security.
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" })); // Configuring CORS policy.
@@ -27,11 +27,9 @@ app.use(morgan("common")); // Logging HTTP requests using the 'common' format.
 app.use(bodyParser.json({ limit: "30mb", extended: true })); // Parsing JSON requests with specified options.
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); // Parsing URL-encoded requests with specified options.
 app.use(cors()); // Enabling Cross-Origin Resource Sharing.
-// Serving static files from the specified directory
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets"))); // Serving static files from the specified directory
 
-// FILE STOREAGE
-
+// FILE STOREAGE //
 // Configure the storage engine using multer.diskStorage
 const storage = multer.diskStorage({
   // Define the destination directory for uploaded files
@@ -41,29 +39,34 @@ const storage = multer.diskStorage({
   },
   // Define the filename for uploaded files
   filename: (req, file, cb) => {
-    // Keep the original filename
-    cb(null, file.originalname);
+    cb(null, file.originalname); // Keep the original filename
   }
 });
-// Create a multer upload middleware instance
-const upload = multer({ storage });
+const upload = multer({ storage }); // Create a multer upload middleware instance
 
-// ROUTES WITH FILES
+// ROUTES WITH FILES //
 app.post("/auth/register", upload.single("picture"), register )
 
-// MONGOOSE SETUP
-
+// MONGOOSE SETUP //
 // Set the port for the application, defaulting to 3000 if not specified in environment variables
 const PORT = process.env.PORT || 3000;
-// Connect to the MongoDB database using Mongoose
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParse: true,  // Ensure compatibility with newer MongoDB connection strings
-    useUnifiedTopology: true, // Use the new MongoDB driver topology engine
-})
-.then(() => {
-    //If connection is successful, start the server and log a message
-    app.listen(PORT, () => console.log(`Connected to DB and listening on Port ${PORT}`));
-})
-.catch((error) => {
-    console.log(`Error: ${error}, did not connect`);
-});
+// Connect to MongoDB using Mongoose
+const connectDB = async () => {
+  try {
+
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParse: true,  // Ensure compatibility with newer MongoDB connection strings
+      useUnifiedTopology: true, // Use the new MongoDB driver topology engine
+    });
+    console.log("connected to MongoDB...");
+    app.listen(PORT, () => {
+      console.log(`Connected to DB and listening on Port ${PORT}`)
+    });
+
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); //Exit the process if database connection fails
+  }
+};
+connectDB(); //initiate connection
