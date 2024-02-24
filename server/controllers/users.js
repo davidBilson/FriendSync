1a514c60f23a1fb5dbf169a7a41631ca2f9d7541
davidBilson <davidbilson99@gmail.com>
2024-02-24 21:42:45 +0100

    fixed indentation on user controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -20,7 +20,14 @@ export const getUserFriends = async (req, res) => {
             user.friends.map((id) => User.findById(id))
         )
         const formattedFriends = friends.map(
-            ({ _id, firstName, lastName, occupation, location, picturePath}) => {
+            ({ 
+                _id, 
+                firstName, 
+                lastName, 
+                occupation, 
+                location, 
+                picturePath
+            }) => {
                 return { _id, firstName, lastName, occupation, location, picturePath};
             }
         );
@@ -45,6 +52,7 @@ export const addRemoveFriend = async (req, res) => {
             user.friends.push(friendId);
             friend.friends.push(id);
         }
+        
         await user.save();
         await friend.save();
 
