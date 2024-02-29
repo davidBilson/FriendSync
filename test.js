893e2b095cb4849328ce66dffdca3299d405f27b
davidBilson <davidbilson99@gmail.com>
2024-02-29 11:23:46 +0100

    Added test files to see difference between filter and includes

---
+++ test.js
@@ -0,0 +1,9 @@
+// includes()
+const testIncludes = "What if it was meant to be?";
+// console.log(testIncludes.includes("meant")); //output is true
+
+// filter()
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// filter out even numbers
+const evenNumbers = numbers.filter((num) => num < 5);
+console.log(evenNumbers);
