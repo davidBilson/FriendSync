21b3cf04503c014ff53e5e83754110bffd3627d7
davidBilson <davidbilson99@gmail.com>
2024-03-02 17:25:19 +0100

    string test

--- test.js
+++ test.js
@@ -1,9 +1,5 @@
-// includes()
-const testIncludes = "What if it was meant to be?";
-// console.log(testIncludes.includes("meant")); //output is true
+let userName = "david" + 7
 
-// filter()
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// filter out even numbers
-const evenNumbers = numbers.filter((num) => num < 5);
-console.log(evenNumbers);
+const calculate = "6" + "5"
+
+console.log(calculate)
