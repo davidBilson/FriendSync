58529ed87a4003a732e0d8d3bbb124529e4daecb
davidBilson <davidbilson99@gmail.com>
2024-03-04 21:43:57 +0100

    configured states using redux and RTK

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
