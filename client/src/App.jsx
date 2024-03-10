dda2f2aa8baa5e933e0b32de678489accdab9006
davidBilson <davidbilson99@gmail.com>
2024-03-10 18:57:50 +0100

    updated config.json & app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import HomePage from 'scenes/homePage/HomePage';
-import LoginPage from 'scenes/loginPage/LoginPage';;
-import ProfilePage from 'scenes/profilePage/ProfilePage';
 import { themeSettings } from './theme';
 import { useSelector } from 'react-redux';
+import LoginPage from './scenes/loginPage/LoginPage';
+import HomePage from './scenes/homePage/HomePage';
+import ProfilePage from './scenes/profilePage/ProfilePage';
 
 const App = () => {
 
@@ -14,11 +14,14 @@ const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Routes> 
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </>
   )
